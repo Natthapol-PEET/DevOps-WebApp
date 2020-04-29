ed667986f136fcb39ff5e33cbe038cee4ba386ea
Natthapol-PEET <natthapol.n@ku.th>
2020-04-29 11:49:32 -0700

    แก้ไข

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ config = {
 mydb = mysql.connector.connect(**config)
 mycursor = mydb.cursor()
 
-UserLogin = 'peet123'
+UserLogin = 'Login'
 PageIndex = 0
 
 def group(data):
@@ -89,18 +89,6 @@ def GET_SUMMARY_fn(date):
 
 @app.route('/')
 def index():
-
-    Smoothie = [   [["Americano Smoothie", "static/coffeeimg/smoothie/americano.jpg", 45],
-                    ["Cappucino Smoothie", "static/coffeeimg/smoothie/cappuccino.jpg", 40],
-                    ["Cocoa Smoothie", "static/coffeeimg/smoothie/cocoashake.jpg", 45],
-                    ["Espresso Smoothie", "static/coffeeimg/smoothie/expresso.jpg", 45]],
-
-                    [["Green Tea Smoothie", "static/coffeeimg/smoothie/Green-Smoothie.jpg", 45],
-                    ["Milk Smoothie", "static/coffeeimg/smoothie/milk-smoothie.jpg", 45],
-                    ["Mocha Smoothie", "static/coffeeimg/smoothie/mocha.jpg", 35],
-                    ["Thai Milk Tea Smoothie", "static/coffeeimg/smoothie/thai-milk-tea.jpg", 35]]
-                ]
-
     TYPE = ['cool', 'hot', 'smoothie']
     COOL, HOT, SMOOTHIE = [], [], []
     j = 0
@@ -121,34 +109,99 @@ def index():
     COOL = group(COOL)
     HOT = group(HOT)
     SMOOTHIE = group(SMOOTHIE)
+    
+    global PageIndex
+    PageIndex = 1
 
     return render_template('index.html', Smoothie=SMOOTHIE, Cool=COOL, Hot=HOT, UserLogin=UserLogin)
 
 @app.route('/login')
 def login():
-    return render_template('login.html', UserLogin=UserLogin)
+    sql = " select uid, name, address, tel_user from user where uid = 'peet123' "
+    mycursor.execute(sql)
+    userdetail = mycursor.fetchall()
+    return render_template('login.html', UserLogin=UserLogin, userdetail=userdetail)
 
-@app.route('/show', methods=['POST'])
+@app.route('/get_login', methods=['POST'])
 def get_login():
     user = request.form['uname']
     pwd = request.form['psw']
-
-    # if user == 'PEET' and pwd == '10042541':
-    #     text = 'Welcome to Shop'
-    # else:
-    #     text = 'Error !!'
 
     uidpass = [user, pwd]
 
     mycursor.execute("SELECT uid, passwd FROM user")
     myresult = mycursor.fetchall()
 
-    if uidpass[0] in array(myresult) and uidpass[1] in array(myresult):
-        text = 'PASS'
-    else:
-        text = 'No PASS'
+    global UserLogin
 
-    return render_template('show.html', text=text, UserLogin=UserLogin)
+    sql = " select uid, name, address, tel_user from user where uid = 'peet123' "
+    mycursor.execute(sql)
+    userdetail = mycursor.fetchall()
+
+    text = ''
+
+    if uidpass[0] in array(myresult) and uidpass[1] in array(myresult):
+        UserLogin = uidpass[0]
+    else:
+        UserLogin = 'Login'
+        return render_template('login.html', UserLogin=UserLogin, text='**Login failed')
+
+    temp = 'index.html'
+    if PageIndex == 1:
+        temp = 'index.html'
+        TYPE = ['cool', 'hot', 'smoothie']
+        COOL, HOT, SMOOTHIE = [], [], []
+        j = 0
+
+        for i in TYPE:
+            sql = "SELECT prod_id, path_img, name, price FROM coffee_data where type like '"+i+"' "
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+
+            if j == 0:
+                COOL = myresult
+            elif j == 1:
+                HOT = myresult
+            else:
+                SMOOTHIE = myresult
+            j += 1
+
+        COOL = group(COOL)
+        HOT = group(HOT)
+        SMOOTHIE = group(SMOOTHIE)
+
+        return render_template(temp, Smoothie=SMOOTHIE, Cool=COOL, Hot=HOT, UserLogin=UserLogin, userdetail=userdetail)
+
+    elif PageIndex == 2:
+        temp = 'index.html'
+        TYPE = ['cool', 'hot', 'smoothie']
+        COOL, HOT, SMOOTHIE = [], [], []
+        j = 0
+
+        for i in TYPE:
+            sql = "SELECT prod_id, path_img, name, price FROM coffee_data where type like '"+i+"' "
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+
+            if j == 0:
+                COOL = myresult
+            elif j == 1:
+                HOT = myresult
+            else:
+                SMOOTHIE = myresult
+            j += 1
+
+        COOL = group(COOL)
+        HOT = group(HOT)
+        SMOOTHIE = group(SMOOTHIE)
+
+        return render_template(temp, Smoothie=SMOOTHIE, Cool=COOL, Hot=HOT, UserLogin=UserLogin, userdetail=userdetail)
+    elif PageIndex == 3:
+        summary()
+    else:
+        return render_template('login.html', UserLogin=UserLogin, userdetail=userdetail)
+
+    
 
 @app.route('/show')
 def get_login2():
@@ -156,40 +209,55 @@ def get_login2():
 
 @app.route('/product_detail', methods=['POST','GET'])
 def product():
-
     id = request.form.get('id')
     sql = "SELECT name, price, type, des, path_img FROM coffee_data where prod_id like '"+id+"' "
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
 
-
     data =[id]
+    
+    global PageIndex
+    PageIndex = 2
 
     return render_template('product_detail.html', data=data, data_info=myresult, UserLogin=UserLogin)
 
 @app.route('/cart')
 def cart():
+    global PageIndex
+    PageIndex = 1
+
     return render_template('cart.html', UserLogin=UserLogin)
 
 @app.route('/invoice')
 def invoice():
-    customer_address = ["Mr.Nonthasri  Nonthasri",
+
+    if UserLogin == 'peet123':
+        customer_address = ["Mr.Nonthasri  Nonthasri",
                         "83/3 Kud Khanuan Bungkaew Subdistrict",
                         "Non Sa-at District",
                         "Udon Thani Province 41240",
                         "093-1851721"]
 
-    customer_address = ["anaphut rattanakham",
+    elif UserLogin == 'peng123':
+        customer_address = ["anaphut rattanakham",
                         "7/3 ban buangdanag",
                         "patumrat roi-et 45190",
                         "",
                         "0649724822"]
 
-    # customer_address = ["natthapol",
-    #                     "8/3 ban nuang jonggram ",
-    #                     "hell 5555",
-    #                     "",
-    #                     "0666131313"]
+    elif UserLogin == 'tong123':
+        customer_address = ["natthapol",
+                     "8/3 ban nuang jonggram ",
+                     "hell 5555",
+                     "",
+                     "0666131313"]
+    else:
+        customer_address = ["",
+                     "",
+                     "",
+                     "",
+                     ""]
+
 
     META = ["000123", "December 15, 2009", 875.00]
 
@@ -199,12 +267,18 @@ def invoice():
 
     STAB = [875.00, 875.00, 0.00, 875.00]
 
+    global PageIndex
+    PageIndex = 1
+
     return render_template('invoice.html', META=META, list_data=values, customer_address=customer_address, STAB=STAB, UserLogin=UserLogin)
 
 @app.route('/summary')
 def summary():
     values = [['coffee', 1, 30], ['Fresh milk', 40, 50], ['Latte', 40, 50]]
     sum = ["summary", 9, 215]
+
+    global PageIndex
+    PageIndex = 3
 
     return render_template('summary.html', data=values, sum=sum, UserLogin=UserLogin)
 
@@ -219,6 +293,15 @@ def get_summary():
     values, sum, sql = GET_SUMMARY_fn(str(birthday))
 
     return render_template('summary.html', data=values, sum=sum, birthday=str(birthday), UserLogin=UserLogin)
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    global UserLogin
+    UserLogin = 'Login'
+
+    return render_template('login.html', UserLogin=UserLogin)
+
+
 
 if __name__== "__main__":
     app.run(debug=True)
